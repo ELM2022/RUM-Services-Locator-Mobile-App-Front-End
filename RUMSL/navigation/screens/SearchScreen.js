2393cffd16fd242b1fdfb9b92a156d25e0caa028
emmastgo <emmanuel.santiago6@upr.edu>
2022-04-10 23:17:16 -0400

    Added Searchbar to Search screen.

--- RUMSL/navigation/screens/SearchScreen.js
+++ RUMSL/navigation/screens/SearchScreen.js
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { View ,Text,Button,Alert} from 'react-native';
+import {  Searchbar } from 'react-native-paper';
+import { View ,Text,Button,Alert,Keyboard} from 'react-native';
 
 const MyComponent = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
   return (
     <View>
-    </View>
+        <Searchbar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+    <Text >
+      Ingrese servicio que esta buscanco...
+    </Text>
+    <Button
+      title="Test Input"
+      onPress={() => Alert.alert(searchQuery) && Keyboard.dismiss} // Value stored in search bar
+    />
+  </View>
   );
 };
 
